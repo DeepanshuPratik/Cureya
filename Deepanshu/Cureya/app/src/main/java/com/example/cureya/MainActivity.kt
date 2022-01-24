a0a702b69bb995b931cefcb0696ba0175e65d234
anmol madaan <Anmolmad503@gmail.com>
2022-01-24 10:48:30 +0530

    camera and share included

--- Deepanshu/Cureya/app/src/main/java/com/example/cureya/MainActivity.kt
+++ Deepanshu/Cureya/app/src/main/java/com/example/cureya/MainActivity.kt
@@ -9,17 +9,98 @@ import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
+import com.example.cureya.adapter.MessageListAdapter
+import com.example.cureya.model.messageInfo
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var madapter: MessageListAdapter
+    private var message =""    // message to be sent
+    private var url = ""        // api url
+    private var sender=""       // sender name
+    private val pickFromGallery:Int = 101
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        url = "TODO"     // Add API HERE.
+        fetchData()
+        madapter = MessageListAdapter()
 
-        /**
-         * Work flow is currently at [ChatBotActivity]
-         */
-        val intent = Intent(this, ChatBotActivity::class.java)
-        this.startActivity(intent)
+        findViewById<ImageView>(R.id.attach).setOnClickListener {
+            attach()
+        }
+        findViewById<ImageView>(R.id.emoji).setOnClickListener {
+            emoji()
+        }
+        findViewById<ImageView>(R.id.camera).setOnClickListener {
+            camera()
+        }
+        findViewById<ImageView>(R.id.mic).setOnClickListener {
+            mic()
+        }
+
+    }
+    private fun fetchData()  {
+
+        val queue = Volley.newRequestQueue(this)
+        val getRequest: JsonObjectRequest = object : JsonObjectRequest(
+            Request.Method.GET,
+            url,
+            null,
+            Response.Listener{
+
+                val chatJsonArray = it.getJSONArray("articles")
+                val messageArray = ArrayList<messageInfo>()
+                for(i in 0 until chatJsonArray.length()){
+                    val chatJsonObject=  chatJsonArray.getJSONObject(i)
+                    val message = messageInfo(
+                        chatJsonObject.getString("title"),
+                        chatJsonObject.getString("author"),
+//                        chatJsonObject.getString("url"),
+//                        chatJsonObject.getString("urlToImage"),
+//                        chatJsonObject.getString("description")
+                    )
+                    messageArray.add(message)
+                }
+                //madapter.updatemessage(newsArray)
+            },
+            Response.ErrorListener {
+
+            }
+        ) {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String> {
+                val params: MutableMap<String, String> = HashMap()
+                params["User-Agent"] = "Mozilla/5.0"
+                return params
+            }
+        }
+
+        queue.add(getRequest)
+    }
+    private fun emoji(){
+
+    }
+    private fun attach(){
+        val intent = Intent()
+        intent.type = "*/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.putExtra("return-data", true)
+        startActivityForResult(Intent.createChooser(intent, "Complete action using"), pickFromGallery)
+    }
+    private fun camera(){
+        val intent = Intent("android.media.action.IMAGE_CAPTURE")
+        startActivity(intent)
+    }
+    private fun mic(){
+
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == pickFromGallery && resultCode == RESULT_OK) {
+            if (data != null) {
+                url = data.data?.toString()!!
+            }
+        }
     }
 }
