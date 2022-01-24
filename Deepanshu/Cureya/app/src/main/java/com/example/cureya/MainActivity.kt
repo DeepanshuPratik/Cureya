b8ce91196d9233dc329b871768034fa56a125040
ronni <ronibainyt@gmail.com>
2022-01-24 23:51:47 +0530

    added navigation graph

--- Deepanshu/Cureya/app/src/main/java/com/example/cureya/MainActivity.kt
+++ Deepanshu/Cureya/app/src/main/java/com/example/cureya/MainActivity.kt
@@ -1,7 +1,27 @@
 package com.example.cureya
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp() || navController.navigateUp()
+    }
 }
