549513c688fbf3fbd6ee2933386e18060a6d5c2b
Divyansh Kushwaha <kushwaha.divyansh.dxn@gmail.com>
2022-02-09 18:54:10 +0530

    comment and like-unlike post

--- Deepanshu/Cureya/app/src/main/java/com/example/cureya/community/ui/fragment/details/PostDetailFragment.kt
+++ Deepanshu/Cureya/app/src/main/java/com/example/cureya/community/ui/fragment/details/PostDetailFragment.kt
@@ -3,6 +3,7 @@ package com.example.cureya.community.ui.fragment.details
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -70,6 +71,7 @@ class PostDetailFragment : Fragment() {
 
     private fun initUi() {
         binding.userImage.load(auth.currentUser!!.photoUrl)
+        binding.postCard.postCardMenuButton.visibility = GONE
         binding.commentsRecycler.apply {
             adapter = commentsRecyclerAdapter
             layoutManager = LinearLayoutManager(requireContext())
